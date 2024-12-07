bb6cf66bdc10f45da7168f51cf86ae7be4771209
qoidnaufal <qoidnaufal@gmail.com>
2024-12-07 16:17:58 +0700

    solved: day 5 part 1 & 2 (with help from Rust)

--- src/day5.rs
+++ src/day5.rs
@@ -3,8 +3,8 @@ use std::collections::HashMap;
 use crate::Error;
 
 pub fn solve(input: &str) -> Result<(), Error> {
-    let parsed_input = parse_input(input).unwrap();
-    parsed_input.solve_part1();
+    let parsed_input = parse_input(input)?;
+    parsed_input.solve();
 
     Ok(())
 }
@@ -16,14 +16,11 @@ struct ParsedInput {
 }
 
 impl ParsedInput {
-    fn solve_part1(&self) {
-        println!("bfr | {:?}", self.must_be_before);
-        println!("aft | {:?}", self.must_be_after);
-        println!("pgs | {:?}\n", self.pages);
+    fn solve(&self) {
+        let mut part1: usize = 0;
+        let mut part2: usize = 0;
 
-        let mut accumulator: usize = 0;
-
-        self.pages.iter().enumerate().for_each(|(line, pages)| {
+        self.pages.iter().enumerate().for_each(|(_, pages)| {
             let mut page_status = true;
             let mut cursor = 0;
             for page in pages {
@@ -39,9 +36,6 @@ impl ParsedInput {
                             afters.contains(order) & rule_b
                         })
                     } else { true };
-                    println!(">> line {line}, cursor {cursor}, [{page}]");
-                    println!("   [{page}] must be before {afters:?}");
-                    println!("   can [{page}] be before {:?} => {status_a}\n", orders);
 
                     page_status &= status_a;
                 }
@@ -51,26 +45,62 @@ impl ParsedInput {
                     let order_can_be_after = if !order.is_empty() {
                         order.iter().any(|n| !befores.contains(n))
                     } else { true };
-                    println!(">> line {line}, cursor {cursor}, [{}],", pages[cursor]);
-                    println!("   is {order:?} not inside {befores:?} => {order_can_be_after}");
-                    println!("   can {order:?} be after [{}] => {order_doesnt_contains_befores}\n", pages[cursor]);
 
                     page_status &= order_doesnt_contains_befores & order_can_be_after;
                 }
-
-                if !page_status { break };
                 cursor += 1 % pages.len();
             }
 
             if page_status {
                 let idx = (pages.len() - 1) / 2;
-                accumulator += pages[idx] as usize;
+                part1 += pages[idx] as usize;
+            } else {
+                reorder(&self.must_be_before, &self.must_be_after, pages, &mut part2);
             }
-            println!("-- line {line} => {page_status}\n");
         });
 
-        println!("part 1 = {accumulator}");
+        // 4957
+        // 6938
+        println!("part 1 = {part1}, part 2 = {part2}");
     }
+}
+
+fn reorder(
+    must_be_before: &HashMap<u8, Vec<u8>>,
+    must_be_after: &HashMap<u8, Vec<u8>>,
+    pages: &Vec<u8>,
+    accumulator: &mut usize
+) {
+    let mut buffer = vec![0; pages.len()];
+
+    pages.iter().enumerate().for_each(|(idx, page)| {
+        let mut cursor: usize = 0;
+        let mut delta = 0;
+        if let Some(befores) = must_be_after.get(page) {
+            let orders = &pages[idx..];
+            if !orders.is_empty() {
+                orders.iter().for_each(|n| {
+                    if befores.contains(n) {
+                        delta += 1;
+                    }
+                })
+            }
+        }
+        if let Some(afters) = must_be_before.get(page) {
+            if idx > 0 {
+                let back_orders = &pages[..idx];
+                afters.iter().for_each(|n| {
+                    if back_orders.contains(n) {
+                        cursor += 1;
+                    }
+                });
+            }
+        }
+        buffer[idx + delta - cursor] = pages[idx];
+        // println!("[{}] -> {cursor} + {delta} - {inner_cursor}", pages[cursor]);
+    });
+
+    *accumulator += buffer[(pages.len() - 1) / 2] as usize;
 }
 
 fn parse_input(input: &str) -> Result<ParsedInput, Error> {
@@ -80,6 +110,7 @@ fn parse_input(input: &str) -> Result<ParsedInput, Error> {
 
     let mut must_be_before = HashMap::<u8, Vec<u8>>::new();
     let mut must_be_after = HashMap::<u8, Vec<u8>>::new();
+
     rule_section.lines().try_for_each(|token| {
         let Ok(num1) = token[..2].parse::<u8>() else { return Err(Error::ParseError) };
         let Ok(num2) = token[3..].parse::<u8>() else { return Err(Error::ParseError) };
@@ -92,11 +123,11 @@ fn parse_input(input: &str) -> Result<ParsedInput, Error> {
         
         if let Some(entry) = must_be_after.get_mut(&num2) {
             entry.push(num1);
-            Ok(())
         } else {
             must_be_after.insert(num2, vec![num1]);
-            Ok(())
         }
+
+        Ok(())
     })?;
 
     let pages = pages_section
